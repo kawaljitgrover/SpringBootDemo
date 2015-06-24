4daa99554a0f569ea76900c7f5b733e57935aa39
Grover, Kawaljit <Kawaljit.Grover@sabre.com>
2015-06-23 21:44:22 -0500

    Spring Boot Demo - updating spring boot version..

--- src/main/java/com/kawal/SpringBootDemoConfig.java
+++ src/main/java/com/kawal/SpringBootDemoConfig.java
@@ -1,16 +1,12 @@
 package com.kawal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@ComponentScan
 public class SpringBootDemoConfig {
 
     @Bean
